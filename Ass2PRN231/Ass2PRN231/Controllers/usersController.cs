763f9652a30a93a0f8e638afd4de3a538b319cfa
minhtriet3110 <90029062+minhtriet3110@users.noreply.github.com>
2024-04-19 17:24:59 +0700

    api user and publisher
    api user and publisher

--- Ass2PRN231/Ass2PRN231/Controllers/usersController.cs
+++ Ass2PRN231/Ass2PRN231/Controllers/usersController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.Execution;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Repository;
 using Repository.Entity;
+using Repository.Models;
 
 namespace Ass2PRN231.Controllers
 {
@@ -14,70 +19,84 @@ namespace Ass2PRN231.Controllers
     public class usersController : ControllerBase
     {
         private readonly Ass2Prn231Context _context;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public usersController(Ass2Prn231Context context)
+        private int? pageSize = 10;
+        private int? currentPage = 1;
+
+        public usersController(Ass2Prn231Context context, UnitOfWork unitOfWork, IMapper mapper)
         {
             _context = context;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
-        // GET: api/users
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        [HttpPost("login")]
+        public async Task<ActionResult<UserModel>> Login([FromBody] Login login)
         {
-          if (_context.Users == null)
-          {
-              return NotFound();
-          }
-            return await _context.Users.ToListAsync();
+            var user = _unitOfWork.UserRepository.Get(m => m.Email.Equals(login.Email) && m.Password.Equals(login.Password)).FirstOrDefault();
+            if (user == null) {
+                return BadRequest("Login error: Email or Password incorrect");
+            }
+
+            var u = _mapper.Map<UserModel>(user);
+            return Ok(u);
         }
+
 
         // GET: api/users/5
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
-          if (_context.Users == null)
-          {
-              return NotFound();
-          }
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
+            if (_unitOfWork.UserRepository == null) {
                 return NotFound();
             }
+            var member = _unitOfWork.UserRepository.GetByID(id);
 
-            return user;
+            if (member == null) {
+                return NotFound();
+            }
+            var result = _mapper.Map<UserModel>(member);
+
+
+            return Ok(result);
         }
 
         // PUT: api/users/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
+        public async Task<IActionResult> PutUser(int id, UserUpdateDTO user)
         {
-            if (id != user.Id)
-            {
-                return BadRequest();
+            var u = _unitOfWork.UserRepository.GetByID(id);
+            if (u == null) {
+                return NotFound();
             }
+            u.Id = user.Id;
+            u.Email = user.Email;
+            u.Password = user.Password;
+            u.Source = user.Source;
+            u.FirstName = user.FirstName;
+            u.LastName = user.LastName;
+            u.RoleId = user.RoleId;
+            u.PubId = user.PubId;
+            u.HireDate = user.HireDate;
 
-            _context.Entry(user).State = EntityState.Modified;
+            _unitOfWork.UserRepository.Update(u);
 
-            try
-            {
-                await _context.SaveChangesAsync();
+            try {
+                _unitOfWork.Save();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
+            catch (DbUpdateConcurrencyException) {
+                if (!UserExists(id)) {
                     return NotFound();
                 }
-                else
-                {
+                else {
                     throw;
                 }
             }
 
-            return NoContent();
+            return Ok();
         }
 
         // POST: api/users
@@ -85,32 +104,41 @@ namespace Ass2PRN231.Controllers
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
-          if (_context.Users == null)
-          {
-              return Problem("Entity set 'Ass2Prn231Context.Users'  is null.");
-          }
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            if (_unitOfWork.UserRepository == null) {
+                return NotFound();
+            }
 
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+           
+            var c = _unitOfWork.UserRepository.GetByID(user.PubId.Value);
+            if (c == null) {
+                return NotFound("Publisher is null");
+            }
+            var p = _mapper.Map<User>(user);
+            _unitOfWork.UserRepository.Insert(p);
+            try {
+                _unitOfWork.Save();
+            }
+            catch (DbUpdateException ex) {
+                return BadRequest(ex);
+            }
+
+            return Ok(user);
         }
 
         // DELETE: api/users/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
-            if (_context.Users == null)
-            {
+            if (_unitOfWork.UserRepository == null) {
                 return NotFound();
             }
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
+            var user = _unitOfWork.UserRepository.GetByID(id);
+            if (user == null) {
                 return NotFound();
             }
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
+            _unitOfWork.UserRepository.Delete(user);
+            _unitOfWork.Save();
 
             return NoContent();
         }
