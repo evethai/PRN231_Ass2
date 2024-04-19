cbcedc96c12e36b15eb3be348d58700f79bb81ea
evethai <117005361+evethai@users.noreply.github.com>
2024-04-19 18:33:13 +0700

    merge api

--- Ass2PRN231/Repository/Entity/Ass2Prn231Context.cs
+++ Ass2PRN231/Repository/Entity/Ass2Prn231Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Repository.Entity;
 
@@ -29,9 +30,21 @@ public partial class Ass2Prn231Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database=Ass2PRN231;User=sa;Password=123456789;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+        var strConn = config["ConnectionStrings:DefaultConnection"];
+        return strConn;
+    }
+
+
+protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Author>(entity =>
         {
